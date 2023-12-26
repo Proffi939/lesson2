b63809a8d86b7183f9f572f91a98db05cccb4123
Proffi939 <152071945+Proffi939@users.noreply.github.com>
2023-12-26 23:09:54 +0500

    убрал комментарии

--- task2.cs
+++ task2.cs
@@ -1,24 +1,24 @@
-// int x = 8;
-// int y = 1;
-// if (x > 0)
-// {
-//     if(y > 0)
-//     {
-//         Console.Write("1");
-//     }
-//     else
-//     {
-//         Console.Write("4");
-//     }
-// }
-// else
-// {
-//     if(y > 0)
-//     {
-//         Console.Write("2");
-//     }
-//     else
-//     {
-//         Console.Write("3");
-//     }
-// }
+int x = 8;
+int y = 1;
+if (x > 0)
+{
+    if(y > 0)
+    {
+        Console.Write("1");
+    }
+    else
+    {
+        Console.Write("4");
+    }
+}
+else
+{
+    if(y > 0)
+    {
+        Console.Write("2");
+    }
+    else
+    {
+        Console.Write("3");
+    }
+}
