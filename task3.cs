32d7467990aacc1525c50a89778e4b87b89ab411
Proffi939 <152071945+Proffi939@users.noreply.github.com>
2023-12-27 00:52:01 +0500

    все работает теперь

--- task3.cs
+++ task3.cs
@@ -1,15 +1,15 @@
-using System.Formats.Asn1;
+// using System.Formats.Asn1;
 
-int n = 59;
-int n1 = n / 10;
-int n2 = n % 10;
-int max = 0;
-if (n1 > n2)
-{
-    max = n1;
-}
-else
-{
-    max = n2;
-}
-Console.Write(max);
+// int n = 59;
+// int n1 = n / 10;
+// int n2 = n % 10;
+// int max = 0;
+// if (n1 > n2)
+// {
+//     max = n1;
+// }
+// else
+// {
+//     max = n2;
+// }
+// Console.Write(max);
